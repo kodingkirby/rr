f1390fb09f7e8cd42a1372ecb1c1dea881b0ba6d
kodingkirby <samkirby818@gmail.com>
2018-07-29 19:24:44 -0700

    added caching to prevent reroll of same restauraunt with 2min ttl. changed search parameters and now selects randomly from top 20 results

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ fs = require('fs')
 var app = express();
 mustache = require('mustache')
 var bodyParser = require('body-parser');
+const NodeCache = require("node-cache");
+const prevResults = new NodeCache();
 require('dotenv').config();
+const ttl = (60 * 2); //2 mins
 
 var path = __dirname + '/views/';
 
@@ -14,7 +17,7 @@ const apiKey = process.env.apiKeyz;
 const client = yelp.client(apiKey);
 
 //==================
-
+var isMatch = "no";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,11 +43,15 @@ var randResult = {};//init the results obj
 var searchRequest = {}; //want to be able to reuse query
 app.post("/process_post", function(req,res){
 
+
+	if (req.body.playlist == "Something Healthy"){
+		req.body.playlist = "Healthy";
+	}
 	searchRequest = {
 		term:req.body.playlist,
 		location:req.body.address,
 		radius:req.body.exampleRadios,
-		limit: 15 //pick randomly from top 15 locations (works up to 50)
+		limit: 20 //pick randomly from top 20 locations (works up to 50)
 	}
 
 	client.search(searchRequest).then(response => {
@@ -55,6 +62,18 @@ app.post("/process_post", function(req,res){
 	  //const template = handlebars.compile(resultSource, { strict: true });
 	  //const result = template(randResult);
 	  //console.log(result);
+	  //prevResults.set()
+
+	  //console.log(randResult.name);
+	  prevResults.set(randResult.alias, randResult, ttl, function(err, success){
+	  	if (!err && success){
+	  		console.log(randResult.alias + " was logged");
+
+	  	}else{
+	  		console.log("nothing was cache");
+	  	}
+	  });
+
 
 	  var rData = {records:randResult};
 	  var page = fs.readFileSync(__dirname + '/views/result.html', "utf8"); // bring in the HTML file
@@ -77,9 +96,43 @@ app.get("/resultjson", function(req,res){
 
 });
 
+app.get("/redirect", function(req,res){
+	console.log("feck");
+	res.redirect('/reroll');
+})
+
 app.get("/reroll", function(req,res){
 	client.search(searchRequest).then(response => {
 	  randResult = response.jsonBody.businesses[Math.floor(Math.random()*response.jsonBody.businesses.length)];
+	  prevResults.keys(function(err,mykeys){
+	  	if(!err){
+	  		console.log("the keys " + mykeys);
+
+	  		for (var i=0; i<mykeys.length; i++){
+	  			if (mykeys[i] == randResult.alias){
+	  				console.log("oh shet its a match");
+	  				isMatch = "yes";
+	  				break;
+	  			}
+	  		}
+	  		if(isMatch == "no"){//cache is there were no matches found
+				prevResults.set(randResult.alias, randResult, ttl, function(err, success){
+				  	if (!err && success){
+				  		console.log(randResult.alias + " was logged");
+				  	}else{
+				  		console.log("nothing was cache");
+		  			}
+		 		 });
+	  		}else if (isMatch == "yes"){//otherwise if there was a match try again
+	  			isMatch = "no"; //put it back for next time
+	  			res.redirect("/reroll");
+	  		}
+
+
+	  	}
+	  });
+
+
 	  console.log(randResult);
 	  console.log("Businesses returned: " + response.jsonBody.businesses.length);
 
